b90f129adcc328013efc8dee421b06ad6a6cd11d
Douman <douman@gmx.se>
2020-01-04 22:22:42 +0100

    0.3.5: Clean up saved games

--- src/rt.rs
+++ src/rt.rs
@@ -42,9 +42,82 @@ pub fn notify(text: &'static str) {
     let _ = unsafe { &*NOTIFY_SEND.as_ptr() }.send(text);
 }
 
+fn caclulate_hash(line: &str) -> u64 {
+    use std::collections::hash_map::DefaultHasher;
+    use std::hash::{Hasher};
+
+    let mut hasher = DefaultHasher::new();
+    hasher.write(line.as_bytes());
+    hasher.finish()
+}
+
+fn clean_up() {
+    use std::io::{BufReader, BufRead, BufWriter, Write};
+    use std::fs;
+    use std::collections::HashSet;
+
+    const SAVE_GAMES: &str = "SavedGames.txt";
+    const SAVE_GAMES_NEW: &str = "SavedGames.txt.new";
+    #[cfg(not(windows))]
+    const NEW_LINE: &[u8] = b"\n";
+    #[cfg(windows)]
+    const NEW_LINE: &[u8] = b"\r\n";
+
+    pub struct RemoveOnDrop<'a>(&'a str);
+    impl<'a> Drop for RemoveOnDrop<'a> {
+        fn drop(&mut self) {
+            let _ = fs::remove_file(self.0);
+        }
+    }
+
+    let file = match fs::File::open(SAVE_GAMES) {
+        Ok(file) => file,
+        _ => return,
+    };
+
+    let mut new_file = match fs::File::create(SAVE_GAMES_NEW) {
+        Ok(file) => BufWriter::new(file),
+        _ => return,
+    };
+
+    let _remove_new_file = RemoveOnDrop(SAVE_GAMES_NEW);
+
+    let mut count = 0;
+    let mut store = HashSet::new();
+
+    for line in BufReader::new(file).lines() {
+        let line = match line {
+            Ok(line) => line,
+            _ => return,
+        };
+
+        match store.insert(caclulate_hash(&line)) {
+            true => match new_file.write(line.as_bytes()).and_then(|_| new_file.write(NEW_LINE)) {
+                Ok(_) => (),
+                _ => return,
+            },
+            false => {
+                count += 1;
+                continue;
+            }
+        }
+    }
+
+    drop(new_file);
+
+    match count {
+        0 => (),
+        _ => match fs::rename(SAVE_GAMES_NEW, SAVE_GAMES) {
+            Ok(_) => drop(_remove_new_file),
+            _ => (),
+        }
+    };
+}
+
 fn init() -> c_int {
     //Access lazy static on load instead of during when processing text
     crate::config::Config::get();
+    clean_up();
 
     1
 }
